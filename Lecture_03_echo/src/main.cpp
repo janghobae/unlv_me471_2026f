1f6e1aa438ec7684ad875bdcac2c1fc102f2b86e
Andrew Bae <baea3@ad.unlv.edu>
2026-07-01 18:14:14 -0700

    Add serial message echo code

---
+++ Lecture_03_echo/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.setTimeout(10);
+}
+
+void loop() {
+    if (Serial.available() > 0) 
+    {
+        String message = Serial.readString();
+        message.trim();
+        Serial.println(message);
+    }
+}
