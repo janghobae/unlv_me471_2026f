7b3649eebe4d8cee495dc168aae2945921afb016
janghobae <andrew.bae@unlv.edu>
2026-08-08 19:53:50 -0700

    Added python list example

---
+++ Lecture_04_PythonBasic/list_grading.py
@@ -0,0 +1,10 @@
+scores = [85, 42, 95, 100, 64, 73]
+
+for numbers in scores:
+    print(numbers)
+
+print("Highets score: ", max(scores))
+
+average = sum(scores) / len(scores)
+
+print("Average: ", average)
