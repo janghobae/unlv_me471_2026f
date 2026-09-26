65566329146805e640f296524b96e2cc66bf04da
Andrew Bae <andrew.bae@unlv.edu>
2026-09-25 22:58:49 -0700

    Revised matplot scatter example

--- Lecture_05_matplotlib/matplot_scatter.py
+++ Lecture_05_matplotlib/matplot_scatter.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
 
-voltage = [0, 1, 2, 3, 4, 5]
-sensor_value = [5, 102, 210, 315, 430, 520]
+time = [1.2, 2.8, 3.4, 4.9, 5.1, 
+        6.7, 7.5, 8.9, 9.3, 10.8,
+        11.4, 12.9, 13.2, 14.7, 15.6, 
+        16.1, 17.8, 18.5, 19.3, 20.7]
+sensor_value = [5.2, 8.1, 7.4, 12.3, 11.6, 
+                15.2, 18.5, 16.8, 21.4, 24.1,
+                22.7, 28.3, 27.6, 31.2, 35.5, 
+                33.9, 38.4, 41.1, 39.7, 45.3]
 
-plt.scatter(voltage, sensor_value)
+plt.scatter(time, sensor_value)
 
-plt.xlabel("Voltage [V]")
-plt.ylabel("Analog Reading")
-plt.title("Voltage vs. Analog Sensor Reading")
+plt.xlabel("Time (s)")
+plt.ylabel("Sensor Value (V)")
+plt.title("Sensor Data")
+
+plt.xlim([0, 25])
+plt.ylim([0, 50])
 
 plt.grid(True)
 plt.show()
